a7c6116c1a7764d30d4c9a1c690832c911e73623
ni910715 <bigpjg0616@gmail.com>
2024-01-07 15:49:17 +0800

    basic form

--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
 # Create your views here.
@@ -13,5 +13,14 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
-    form = CreateNewList()
+    if response.method == "POST":
+        form = CreateNewList(response.POST) # response.POST會返回在form中的所有key value成一個字典
+
+        if form.is_valid():
+            n = form.cleaned_data["name"] # cleaned_data解密 並使用字典的方式取出name的value
+            t = ToDoList(name = n)
+            t.save()
+        return HttpResponseRedirect("/%i" %t.id) # 重新導向新的url
+    else:
+        form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
