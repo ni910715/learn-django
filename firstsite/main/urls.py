673f29d0e012fc2e789622b2be08214b51e3a660
ni910715 <bigpjg0616@gmail.com>
2024-01-07 13:53:41 +0800

    add forms

--- firstsite/main/urls.py
+++ firstsite/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"), # 網址後完全不打路徑所看見的view
-    path("", views.home, name="home") # setup home page
+    path("", views.home, name="home"), # setup home page
+    path("create/", views.create, name="create"), # add the create views in path
 ]
--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 # 定義不同網址時所看到的畫面
@@ -10,3 +11,7 @@ def index(response, id):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
