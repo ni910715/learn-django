3b171c5e1e61254c21270d8a6f91ce6961fed320
ni910715 <bigpjg0616@gmail.com>
2024-01-19 10:34:05 +0800

    let user can only look for their todolist

--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -7,24 +7,26 @@ from .forms import CreateNewList
 # 定義不同網址時所看到的畫面
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new") # new是輸入框所輸入的字
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new") # new是輸入框所輸入的字
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
+                    print("invalid")
+    
 
-
-    return render(response, "main/list.html", {"ls":ls})
+        return render(response, "main/list.html", {"ls":ls})
+    return render(response, "main/view.html", {})
 
 def home(response):
     return render(response, "main/home.html", {})
